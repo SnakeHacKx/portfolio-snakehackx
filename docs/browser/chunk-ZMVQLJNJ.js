7f97502421300c1a43e4a0ee463842297bd9f0c8
SnakeHacKx <omaralexisxdx2@gmail.com>
2024-04-18 13:49:45 -0500

    Docs created

---
+++ docs/browser/chunk-ZMVQLJNJ.js
@@ -0,0 +1,1 @@
+import{J as m,L as n,a as r,c as e,u as i}from"./chunk-BPEQWYLA.js";var g=(()=>{let o=class o{constructor(){console.log("ProjectsModule loaded")}};o.\u0275fac=function(c){return new(c||o)},o.\u0275mod=e({type:o}),o.\u0275inj=r({imports:[i,m,n]});let t=o;return t})();export{g as ProjectsModule};
