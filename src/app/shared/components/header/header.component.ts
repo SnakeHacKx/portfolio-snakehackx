ef0123abd24de98f7b9e806822c89c795f22a450
SnakeHacKx <omaralexisxdx2@gmail.com>
2024-04-20 14:31:42 -0500

    before adding responsiveness to project slider

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'shared-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(private sharedService: SharedService) {
+  isMobile = false;
+  isMenuOpen = false;
 
+  constructor(private sharedService: SharedService) {
+    this.checkScreenSize(window.innerWidth);
   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize(event.target.innerWidth);
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
   goToSection(sectionId: string) {
     this.sharedService.scrollToSection(sectionId);
-   }
+    if (this.isMobile) {
+      this.isMenuOpen = false;
+    }
+  }
+
+  checkScreenSize(width: number) {
+    this.isMobile = width < 600;
+  }
 }
